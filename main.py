a8986900032deabd44a2ad6973004a757cdf057a
abraam-atef <atefpeo@gmail.com>
2025-11-17 10:44:04 +0200

    Implement main block for Uvicorn server
    Added a main block to run the application with Uvicorn.

--- main.py
+++ main.py
@@ -134,3 +134,8 @@ def update(qid: int, updated: CreateQuestion, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(q)
     return q
+
+import uvicorn, os
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
